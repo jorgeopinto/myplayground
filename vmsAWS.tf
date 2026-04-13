7b5e201e4d84fcebcc2c266b3ed35bc317f32ed8
Jorge Oliveira Pinto <jorgeopinto@gmail.com>
2026-04-13 15:01:54 +0100

    update terraform yml

--- vmsAWS.tf
+++ vmsAWS.tf
@@ -5,7 +5,7 @@ resource "aws_key_pair" "key" {
 
 resource "aws_instance" "vm" {
   ami           = "ami-05852c5f195d545ea"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name = aws_key_pair.key.key_name
   subnet_id = aws_subnet.Subnet_linux.id
   vpc_security_group_ids = [aws_security_group.sg-access-to-linux.id]
