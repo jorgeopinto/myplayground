0b9f7db48bdf78610d41cd65379a9fe1297759be
Jorge Oliveira Pinto <jorgeopinto@gmail.com>
2026-04-21 12:26:56 +0100

    update terraform file system

--- environments/dev/azure/variables.tf
+++ environments/dev/azure/variables.tf
@@ -1,63 +1,69 @@
 variable "azure_key_pub" {
   description = "chave public para maquina Azure"
   type        = string
-
 }
 
+#─────────────────────────────────────
+#Declarar variaveis a usar para um HUB
+#─────────────────────────────────────
 
 variable "location" {
   description = "Região Azure para todos os recursos"
   type        = string
-  default     = "westeurope"
+  #default     = "westeurope"
 }
 
 variable "hub_resource_group_name" {
   description = "Nome do Resource Group do Hub"
   type        = string
-  default     = "rg-hub-network"
+  #default     = "rg-hub-network"
 }
 
 variable "hub_vnet_name" {
   description = "Nome da VNet Hub"
   type        = string
-  default     = "vnet-hub"
+  #default     = "vnet-hub"
 }
 
 variable "hub_address_space" {
   description = "Bloco CIDR da VNet Hub"
   type        = string
-  default     = "10.0.0.0/16"
+  #default     = "10.0.0.0/16"
 }
 
 variable "hub_gateway_subnet_prefix" {
   description = "Prefixo da GatewaySubnet (obrigatório para VPN/ExpressRoute)"
   type        = string
-  default     = "10.0.0.0/26"
+  #default     = "10.0.0.0/26"
 }
 
 variable "hub_firewall_subnet_prefix" {
   description = "Prefixo da AzureFirewallSubnet"
   type        = string
-  default     = "10.0.1.0/26"
+  #default     = "10.0.1.0/26"
 }
 
 variable "hub_management_subnet_prefix" {
   description = "Prefixo da subnet de gestão"
   type        = string
-  default     = "10.0.2.0/24"
+  #default     = "10.0.2.0/24"
 }
 variable "hub_nva_subnet_prefix" {
   description = "Prefixo da subnet de uma NVA"
   type        = string
-  default     = "10.0.3.0/24"
+  #default     = "10.0.3.0/24"
 }
 
 
 variable "enable_gateway_transit" {
   description = "Activar gateway transit (requer VPN Gateway no Hub)"
   type        = bool
-  default     = false
+  #default     = false
 }
+
+#─────────────────────────────────────
+#Declarar variaveis a usar para um HUB
+#─────────────────────────────────────
 
 variable "spokes" {
   description = "Mapa de configurações dos Spokes"
@@ -72,6 +78,11 @@ variable "spokes" {
     }))
   }))
 }
+
+#─────────────────────────────────────
+#Declarar variaveis a usar para um HUB
+#─────────────────────────────────────
+
 
 variable "common_tags" {
   description = "Tags comuns a todos os recursos"
